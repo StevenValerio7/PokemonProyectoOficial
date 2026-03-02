2bd85a322ecc188f806b7adf5c794e97e293cc7a
StevenValerio7 <stevenvalerio7@gmail.com>
2026-03-02 01:32:11 -0600

    primer commit

---
+++ src/main/java/com/pokemonprojecto/PokemonProjecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pokemonprojecto;
+
+/**
+ *
+ * @author steve
+ */
+public class PokemonProjecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
