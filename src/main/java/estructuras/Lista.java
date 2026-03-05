c54dadd7c0de7501e016abea89d63031daa2ac05
arijimenezz <arianajimenezuni@gmail.com>
2026-03-05 11:45:57 -0600

    Create Lista.java

---
+++ src/main/java/estructuras/Lista.java
@@ -0,0 +1,51 @@
+package estructuras;
+
+import modelo.Pokemon;
+
+
+public class Lista {
+    private Nodo cabeza;
+
+    public Lista() {
+        cabeza = null;
+    }
+
+    public void agregar(Pokemon p) {
+        Nodo nuevo = new Nodo(p);
+        if (cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            Nodo actual = cabeza;
+            while (actual.siguiente != null) {
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevo;
+        }
+    }
+
+    public Pokemon obtener(int indice) {
+        Nodo actual = cabeza;
+        int contador = 0;
+        while (actual != null) {
+            if (contador == indice) {
+                return actual.pokemon;
+            }
+            actual = actual.siguiente;
+            contador++;
+        }
+        return null;
+    }
+
+    public void mostrarCatalogo() {
+        Nodo actual = cabeza;
+        int i = 0;
+        while (actual != null) {
+            System.out.println(i + ". " + actual.pokemon.toString());
+            actual = actual.siguiente;
+            i++;
+        }
+    }
+}
+
+    
+
