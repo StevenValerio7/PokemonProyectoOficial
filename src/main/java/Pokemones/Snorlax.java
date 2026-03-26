19fd05cc09dbbbecdbbcc4213517edbfb3776455
arijimenezz <arianajimenezuni@gmail.com>
2026-03-25 18:16:59 -0600

    Create Snorlax.java

---
+++ src/main/java/Pokemones/Snorlax.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pokemones;
+
+import modelo.*;
+
+/**
+ *
+ * @author jimen
+ */
+public class Snorlax extends PokemonNormal{
+    
+     public Snorlax() {
+        super("Pikachu");
+    }
+}
