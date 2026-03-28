d7bf625f99afd2681ac9a5f0533aef74d611496d
arijimenezz <arianajimenezuni@gmail.com>
2026-03-27 21:26:01 -0600

    Update PantallaBienvenida.java

--- src/main/java/principal/PantallaBienvenida.java
+++ src/main/java/principal/PantallaBienvenida.java
@@ -7,7 +7,7 @@ package principal;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
-
+import gui.SeleccionPokemonGUI;
 
 public class PantallaBienvenida extends javax.swing.JFrame {
 
@@ -101,9 +101,8 @@ public class PantallaBienvenida extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         detenerMusica(); // Detener la música antes de cambiar de pantalla
-        BarraProgreso progreso = new BarraProgreso();
-        progreso.setVisible(true);
-        progreso.iniciarCarga();
+        SeleccionPokemonGUI seleccion = new SeleccionPokemonGUI();
+        seleccion.setVisible(true);
         dispose(); // Cerrar la pantalla actual
     }//GEN-LAST:event_jButton1ActionPerformed
 
